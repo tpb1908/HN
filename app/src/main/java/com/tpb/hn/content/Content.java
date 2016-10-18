12c9aae26e8fc4ca8555e506d861279c991603d4
tpb1908 <theo1908phone@gmail.com>
2016-10-18 20:10:56 +0100

    Set up main layouts for content and current story.

--- app/src/main/java/com/tpb/hn/content/Content.java
+++ app/src/main/java/com/tpb/hn/content/Content.java
@@ -2,7 +2,12 @@ package com.tpb.hn.content;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 
 import com.androidnetworking.AndroidNetworking;
@@ -12,6 +17,8 @@ import com.androidnetworking.interfaces.StringRequestListener;
 import com.tpb.hn.R;
 import com.tpb.hn.network.APIPaths;
 
+import butterknife.BindView;
+
 /**
  * Created by theo on 17/10/16.
  */
@@ -19,12 +26,19 @@ import com.tpb.hn.network.APIPaths;
 public class Content extends AppCompatActivity {
     private static final String TAG = Content.class.toString();
 
+    @BindView(R.id.story_toolbar)
+    Toolbar mToolbar;
+
+    @BindView(R.id.viewpager)
+    ViewPager pager;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_content);
-        AndroidNetworking.initialize(getApplicationContext());
+        setSupportActionBar(mToolbar);
 
+        AndroidNetworking.initialize(getApplicationContext());
         AndroidNetworking.get(APIPaths.getNewStoriesPath())
                 .setTag("test")
                 .setPriority(Priority.HIGH)
@@ -42,8 +56,33 @@ public class Content extends AppCompatActivity {
                 });
 
 
+        ViewPager p = (ViewPager) findViewById(R.id.viewpager);
 
+        p.setAdapter(new Adapter(getSupportFragmentManager()));
 
+        ((TabLayout) findViewById(R.id.tabs)).setupWithViewPager(p);
+    }
+
+    private class Adapter extends FragmentPagerAdapter {
+
+        public Adapter(android.support.v4.app.FragmentManager manager) {
+            super(manager);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return new Fragment();
+        }
+
+        @Override
+        public int getCount() {
+            return 3;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return  "Title";
+        }
     }
 
 }
