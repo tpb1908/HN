94d2e01e85ba1790fd92cc768a7d4076ce2e6adb
tpb1908 <theo1908phone@gmail.com>
2016-10-20 17:48:06 +0100

    Added better caching to the ReadabilityLoader. Listeners are added to a static  HashMap, and the GET is only called if the HashMap is empty. When getAsJSONObject is called, it updates all of the listeners. This means that any GET calls for a URL which is already being loaded will all return from the same GET.

--- app/src/main/java/com/tpb/hn/story/ReadabilityLoader.java
+++ app/src/main/java/com/tpb/hn/story/ReadabilityLoader.java
@@ -10,6 +10,7 @@ import com.tpb.hn.network.APIPaths;
 
 import org.json.JSONObject;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 /**
@@ -20,6 +21,7 @@ import java.util.HashMap;
 public class ReadabilityLoader {
     private static final String TAG = ReadabilityLoader.class.getCanonicalName();
     private static HashMap<String, JSONObject> cache = new HashMap<>();
+    private static HashMap<String, ArrayList<ReadabilityLoadDone>> listenerCache = new HashMap<>();
     private ReadabilityLoadDone listener;
 
     public ReadabilityLoader(ReadabilityLoadDone listener) {
@@ -30,30 +32,42 @@ public class ReadabilityLoader {
         if(cache.containsKey(url)) {
             listener.loadDone(cache.get(url), true);
         }
-        final long start = System.nanoTime();
-        AndroidNetworking.get(APIPaths.getReadabilityParserPath(url))
-                .setTag("readability")
-                .setPriority(Priority.HIGH)
-                .build()
-                .getAsJSONObject(new JSONObjectRequestListener() {
-                    @Override
-                    public void onResponse(JSONObject response) {
-                        try {
-                            Log.d(TAG, "onResponse: " + (System.nanoTime()-start)/1E9);
-                            listener.loadDone(response, response != null);
-                            if(response != null) {
-                                cache.put(url, response);
+        if(listenerCache.containsKey(url)) {
+            listenerCache.get(url).add(listener);
+        } else {
+            final ArrayList<ReadabilityLoadDone> listeners = new ArrayList<>();
+            listeners.add(listener);
+            listenerCache.put(url, listeners);
+            final long start = System.nanoTime();
+            AndroidNetworking.get(APIPaths.getReadabilityParserPath(url))
+                    .setTag("readability")
+                    .setPriority(Priority.HIGH)
+                    .build()
+                    .getAsJSONObject(new JSONObjectRequestListener() {
+                        @Override
+                        public void onResponse(JSONObject response) {
+                            try {
+                                Log.d(TAG, "onResponse: " + (System.nanoTime()-start)/1E9);
+                                Log.i(TAG, "onResponse: updating " + listenerCache.get(url).size() + " listeners");
+                                for(ReadabilityLoadDone rld : listenerCache.get(url)) {
+                                    rld.loadDone(response, response != null);
+                                }
+                                listenerCache.remove(url);
+                                if(response != null) {
+                                    cache.put(url, response);
+                                }
+                            } catch(Exception e) {
+                                Log.e(TAG, "onResponse: ", e);
                             }
-                        } catch(Exception e) {
-                            Log.e(TAG, "onResponse: ", e);
                         }
-                    }
 
-                    @Override
-                    public void onError(ANError anError) {
-                        Log.e(TAG, "onError: ", anError );
-                    }
-                });
+                        @Override
+                        public void onError(ANError anError) {
+                            Log.e(TAG, "onError: ", anError );
+                        }
+                    });
+        }
+
     }
 
     private void stripImageCopyright() {}
