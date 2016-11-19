31ed0438159f59c39a02e18eb3a33aae3660dfe5
tpb1908 <theo1908phone@gmail.com>
2016-11-19 12:24:06 +0000

    Added the skeleton for a settings activity.

--- app/src/main/java/com/tpb/hn/SettingsActivity.java
+++ app/src/main/java/com/tpb/hn/SettingsActivity.java
@@ -1,98 +1,78 @@
 package com.tpb.hn;
 
-import android.content.Context;
-import android.content.res.Configuration;
-import android.graphics.Color;
 import android.os.Bundle;
-import android.preference.PreferenceActivity;
-import android.support.design.widget.AppBarLayout;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import java.util.List;
+
+import butterknife.BindView;
+import butterknife.BindViews;
+import butterknife.ButterKnife;
 
 /**
  * Created by theo on 28/10/16.
  */
 
-public class SettingsActivity extends PreferenceActivity {
+public class SettingsActivity extends AppCompatActivity {
+    private static final String TAG = SettingsActivity.class.getSimpleName();
 
-    @Override
+    @BindViews({R.id.title_settings_theme, R.id.title_settings_content, R.id.title_settings_comments, R.id.title_settings_browser, R.id.title_settings_data, R.id.title_settings_info}) List<TextView> mSettingsTitles;
+    @BindViews({R.id.settings_theme, R.id.settings_content, R.id.settings_comments, R.id.settings_browser, R.id.settings_data, R.id.settings_info }) List<RelativeLayout> mSettings;
+    @BindView(R.id.toolbar) Toolbar mToolbar;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        final LinearLayout root = (LinearLayout)findViewById(android.R.id.list).getParent().getParent().getParent();
-        AppBarLayout bar = (AppBarLayout) LayoutInflater.from(this).inflate(R.layout.settings_toolbar, root, false);
-        root.addView(bar, 0); // insert at top
-        ((Toolbar) bar.findViewById(R.id.toolbar)).setNavigationOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                finish();
-            }
-        });
-        getListView().setBackgroundColor(getColor(R.color.md_grey_bg));
-        getListView().setCacheColorHint(Color.TRANSPARENT);
-
+        setContentView(R.layout.activity_settings);
+        ButterKnife.bind(this);
+        ButterKnife.apply(mSettingsTitles, TOGGLE);
+        setSupportActionBar(mToolbar);
     }
 
+    private final ButterKnife.Action<View> TOGGLE = new ButterKnife.Action<View>() {
+        int current = -1;
+        @Override
+        public void apply(@NonNull View view, final int index) {
+            view.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    if(current == -1) {
+                        show(mSettings.get(index));
+                    } else if(current == index){
+                        if(mSettings.get(index).getVisibility() == View.VISIBLE) {
+                            hide(mSettings.get(index));
+                        } else {
+                            show(mSettings.get(index));
+                        }
+                    } else {
+                        hide(mSettings.get(current));
+                        show(mSettings.get(index));
+
+                    }
+                    current = index;
+                }
+            });
+
+        }
+
+        void show(@NonNull final View view) {
+            view.setVisibility(View.VISIBLE);
+        }
+
+        void hide(@NonNull final View view) {
+            view.setVisibility(View.GONE);
+        }
+
+    };
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        final int id = item.getItemId();
-        if (id == android.R.id.home) {
-            onBackPressed();
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-
-    }
-
-    private void setupActionBar() {
-        final android.app.ActionBar actionBar = getActionBar();
-        if(actionBar != null) {
-            actionBar.setDisplayHomeAsUpEnabled(true);
-        }
-    }
-
-    @Override
-    public boolean onIsMultiPane() {
-        return isLargeTablet(this);
-    }
-
-    private static boolean isLargeTablet(Context context) {
-        return (context.getResources().getConfiguration().screenLayout
-                & Configuration.SCREENLAYOUT_SIZE_MASK) >= Configuration.SCREENLAYOUT_SIZE_LARGE;
-    }
-
-    @Override
-    public void onBuildHeaders(List<PreferenceActivity.Header> target) {
-        loadHeadersFromResource(R.xml.preference_headers, target);
-    }
-
-    @Override
-    protected boolean isValidFragment(String fragmentName) {
-        return PreferenceFragment.class.getName().equals(fragmentName);
-    }
-
-    public static class PreferenceFragment extends android.preference.PreferenceFragment {
-
-        @Override
-        public void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            final String settings = getArguments().getString("settings");
-            switch(settings.toLowerCase()) {
-                case "prefs_theme":
-                    addPreferencesFromResource(R.xml.preferences_theme);
-                    break;
-                case "prefs_content":
-                    addPreferencesFromResource(R.xml.preferences_content);
-                    break;
-            }
-        }
-
-
-
+        onBackPressed();
+        return true;
     }
 }
